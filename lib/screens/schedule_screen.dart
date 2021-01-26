4dba72578722b1b1596a7478ef996077fe04badd
mahendra <mahendrachandel05@gmail.com>
2021-01-26 22:12:19 +0530

    * Calender animation added in schedule screen.

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -10,56 +10,92 @@ class ScheduleScreen extends StatefulWidget {
   _ScheduleScreenState createState() => _ScheduleScreenState();
 }
 
-class _ScheduleScreenState extends State<ScheduleScreen> {
+class _ScheduleScreenState extends State<ScheduleScreen> with TickerProviderStateMixin {
   final DropDownController _dateController = DropDownController();
   final DropDownController _timeController = DropDownController();
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 200,
-              ),
-              Text(
-                Strings.scheduleHeading,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                Strings.scheduleDesc,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.black,
-                  height: 22 / 14,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Container(
+                  decoration: ShapeDecoration(
+                    color: Colors.white.withOpacity(0.3),
+                    shape: CircleBorder(),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0 * _animationController.value),
+                    child: child,
+                  ),
+                );
+              },
+              child: Container(
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    _startAnimation();
+                  },
+                  color: Colors.blue,
+                  icon: Icon(Icons.calendar_today, size: 24),
                 ),
               ),
-              SizedBox(
-                height: 40,
+            ),
+            Text(
+              Strings.scheduleHeading,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              Strings.scheduleDesc,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+                height: 22 / 14,
               ),
-              DropDown(
-                items: ['Date1', 'Date2', 'Date3', 'Date4'],
-                hintText: 'Date',
-                controller: _dateController,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              DropDown(
-                items: ['Time1', 'Time2', 'Time3'],
-                hintText: 'Time',
-                controller: _timeController,
-              ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            DropDown(
+              items: ['Date1', 'Date2', 'Date3', 'Date4'],
+              hintText: 'Date',
+              controller: _dateController,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            DropDown(
+              items: ['Time1', 'Time2', 'Time3'],
+              hintText: 'Time',
+              controller: _timeController,
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: Padding(
@@ -83,5 +119,11 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
         ),
       ),
     );
+  }
+
+  _startAnimation() {
+    if (_animationController == null) return;
+    _animationController.forward();
+    _animationController.repeat(reverse: true);
   }
 }
